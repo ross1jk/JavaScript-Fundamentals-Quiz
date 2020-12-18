40e92a30fc46bd95bc17584fec4d46b6aa83d251
Jacqueline Ross <jacqueline.ross09@gmail.com>
2020-12-17 21:35:48 -0500

    added card ids to html and refrenced those ids in javascript document. hid all cards, besides intro to quiz card

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+//card varibles
+var intro = document.querySelector("#intro"); 
+var qu1El = document.querySelector("#qu1");
+var qu2El = document.querySelector("#qu2");
+var qu3El = document.querySelector("#qu3");
+var qu4El = document.querySelector("#qu4");
+var qu5El = document.querySelector("#qu5");
+var completeEl = document.querySelector("#complete");
+var highScoreEl = document.querySelector("#highscore");
+
+//hidden cards to start only intro is showing
+qu1El.style.display = "none";
+qu2El.style.display = "none";
+qu3El.style.display = "none";
+qu4El.style.display = "none";
+qu5El.style.display = "none";
+completeEl.style.display = "none";
+highScoreEl.style.display = "none";
