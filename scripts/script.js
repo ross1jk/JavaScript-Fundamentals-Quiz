f394e3b8ef31e87c9b0ad02ea8e72f6fe721d7ae
Jacqueline Ross <jacqueline.ross09@gmail.com>
2020-12-20 11:12:42 -0500

    added varibles I may needed based with reasons why I may need them and where I may use them in the future

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,21 @@
-/* Varibles Needed:
-position to show current postion 
-number correct 
-var to html: test and test status
-question 
-choice options
-choices 
-for each answer 
-
-Array with objects needed:
-    Question: gives the question
-    ans1: gives the answers 
-    ans2
-    ans3
-    ans4
-    answer: gives actual answer to refrence 
-
-
-function to refrence how to return elements to the document 
-
-function for rendering the question with the answers
-each answer will be a button linked with an id for a value to match the answer in the array 
-
-function to check answer 
-
-re-render question
-
-
-will need a start screen before
-a score page
-a highscore list 
-store to local storage 
-able to clear socres on local storage and navigate back to start 
-*/
-
+//used to say what position the user is at in the test 
+var pos = 0; 
+//stores answers correct 
+var correct = 0; 
+//test question
+var test;
+//test answers area
+var testStatus; 
+//will refrence my question and choices
+var question; 
+var choice; 
+var choices; 
+//answers that the user will see as poitions to pick from 
+var uAns1;
+var uAns2; 
+var uAns3; 
+var uAns4; 
+//question and answer array
 var questions = [
     {
       question: "Commonly used data types DO NOT include:",
@@ -75,3 +58,38 @@ var questions = [
       answer: "1", 
     }
   ];
+
+/* Varibles Needed:
+position to show current postion 
+number correct 
+var to html: test and test status
+question 
+choice options
+choices 
+for each answer 
+
+Array with objects needed:
+    Question: gives the question
+    ans1: gives the answers 
+    ans2
+    ans3
+    ans4
+    answer: gives actual answer to refrence 
+
+
+function to refrence how to return elements to the document 
+
+function for rendering the question with the answers
+each answer will be a button linked with an id for a value to match the answer in the array 
+
+function to check answer 
+
+re-render question
+
+
+will need a start screen before
+a score page
+a highscore list 
+store to local storage 
+able to clear socres on local storage and navigate back to start 
+*/
